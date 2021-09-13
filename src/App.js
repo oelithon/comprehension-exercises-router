31c86c9cfae0c0ec9f595707455c1b040a54775c
Elithon Silva <elithonsilva@gmail.com>
2021-09-13 15:26:45 -0300

    Adicione uma rota que mapeie o caminho de URL "/about" para o componente About.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/" component={ Home } />
+        <Route path="/about" component={ About } />
       </BrowserRouter>
     );
   }
